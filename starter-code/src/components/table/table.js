603aaa11ea59212c8d98b18fd43cb61c7d7e4dc1
Dazpel <alexxdav97@gmail.com>
2020-04-21 14:44:00 -0400

    fixed width

--- starter-code/src/components/table/table.js
+++ starter-code/src/components/table/table.js
@@ -13,7 +13,7 @@ import { makeStyles } from '@material-ui/core/styles';
 
 export default class IronContacts extends Component {
   state = {
-    list: contacts.slice(0, 5)
+    list: contacts.slice(0, 5),
   };
 
   addContact = () => {
@@ -22,78 +22,88 @@ export default class IronContacts extends Component {
   };
 
   sortName = () => {
-    let currentArr = [...this.state.list].sort(function(a, b){return (a.name).localeCompare(b.name)})
-    this.setState({ list: currentArr});
-  }
+    let currentArr = [...this.state.list].sort(function (a, b) {
+      return a.name.localeCompare(b.name);
+    });
+    this.setState({ list: currentArr });
+  };
 
   sortPopularity = () => {
-    let currentArr = [...this.state.list].sort(function(a, b){return b.popularity - a.popularity})
-    this.setState({ list: currentArr});
-  }
+    let currentArr = [...this.state.list].sort(function (a, b) {
+      return b.popularity - a.popularity;
+    });
+    this.setState({ list: currentArr });
+  };
 
   deleteThis = (id) => {
-    let currentArr = [...this.state.list]
-    currentArr.splice(id, 1)
-    this.setState({ list: currentArr});
-  }
+    let currentArr = [...this.state.list];
+    currentArr.splice(id, 1);
+    this.setState({ list: currentArr });
+  };
 
   render() {
     return (
       <>
-      <div className="container">
-        <h1>React Lab I: IronContacts</h1>
-      <div className="Btns">
-          <Button
-            variant="contained"
-            color="primary"
-            href="#contained-buttons"
-            onClick={this.addContact}
-          >
-            Add Random Contact
-          </Button>
-          <Button variant="outlined" onClick={this.sortName}>Sort by Name</Button>
-          <Button variant="outlined" onClick={this.sortPopularity}>Sort by Popularity</Button>
-        </div>
-        <TableContainer className="table" component={Paper}>
-          <Table  aria-label="simple table">
-            <TableHead>
-              <TableRow className="table-top">
-                <TableCell>Picture</TableCell>
-                <TableCell align="right">Name</TableCell>
-                <TableCell align="right">Popularity</TableCell>
-                <TableCell align="right">Action</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {this.state.list.map((row, index) => (
-                <TableRow key={index}>
-                  <TableCell component="th" scope="row">
-                    <img
-                      src={row.pictureUrl}
-                      alt={row.name}
-                      width="70"
-                      height="70"
-                    />
-                  </TableCell>
-                  <TableCell align="right">{row.name}</TableCell>
-                  <TableCell align="right">{row.popularity.toFixed(2)}</TableCell>
-                  <TableCell align="right">
-                    <Button
-                      variant="contained"
-                      color="secondary"
-                      href="#contained-buttons"
-                      onClick={() => this.deleteThis(index)}
-                    >
-                      Delete
-                    </Button>
-                  </TableCell>
+        <div className="container">
+          <h1>React Lab I: IronContacts</h1>
+          <div className="Btns">
+            <Button
+              variant="contained"
+              color="primary"
+              href="#contained-buttons"
+              onClick={this.addContact}
+            >
+              Add Random Contact
+            </Button>
+            <Button variant="outlined" onClick={this.sortName}>
+              Sort by Name
+            </Button>
+            <Button variant="outlined" onClick={this.sortPopularity}>
+              Sort by Popularity
+            </Button>
+          </div>
+          <TableContainer className="table" component={Paper}>
+            <Table aria-label="simple table">
+              <TableHead>
+                <TableRow className="table-top">
+                  <TableCell>Picture</TableCell>
+                  <TableCell align="right">Name</TableCell>
+                  <TableCell align="right">Popularity</TableCell>
+                  <TableCell align="right">Action</TableCell>
                 </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
-      </div>
-        
+              </TableHead>
+              <TableBody>
+                {this.state.list.map((row, index) => (
+                  <TableRow key={index}>
+                    <TableCell component="th" scope="row">
+                      <img
+                        src={row.pictureUrl}
+                        alt={row.name}
+                        width="70"
+                        height="70"
+                        borderRadious="50"
+                      />
+                    </TableCell>
+                    <TableCell align="right">{row.name}</TableCell>
+                    <TableCell align="right">
+                      {row.popularity.toFixed(2)}
+                    </TableCell>
+                    <TableCell align="right">
+                      <Button
+                        variant="contained"
+                        color="secondary"
+                        href="#contained-buttons"
+                        onClick={() => this.deleteThis(index)}
+                      >
+                        Delete
+                      </Button>
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </div>
       </>
     );
   }
